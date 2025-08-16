a61a9b61c6f0018c150df8554af37067aec359ff
Muhammad Mustajeeb <mprogramming48@gmail.com>
2025-08-16 07:10:15 -0700

    Refactor ImageWithFallback component to improve readability by formatting img tags

--- src/components/figma/ImageWithFallback.tsx
+++ src/components/figma/ImageWithFallback.tsx
@@ -19,10 +19,24 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
       style={style}
     >
       <div className="flex items-center justify-center w-full h-full">
-        <img src={ERROR_IMG_SRC} alt="Error loading image" {...rest} data-original-url={src} />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={ERROR_IMG_SRC}
+          alt="Error loading image"
+          {...rest}
+          data-original-url={src}
+        />
       </div>
     </div>
   ) : (
-    <img src={src} alt={alt} className={className} style={style} {...rest} onError={handleError} />
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={src}
+      alt={alt}
+      className={className}
+      style={style}
+      {...rest}
+      onError={handleError}
+    />
   )
 }
